2a5e7114f5a49f5b729ff7039c3f69d8167917da
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-06-27 15:35:22 +0200

    quotes

--- install.py
+++ install.py
@@ -26,36 +26,36 @@ os.system("""sudo apt-get install build-essential \
     python3-numpy \
     zip""")
 
-os.system("python3 -m venv %s/slave_venv" % curpath)
+os.system("python3 -m venv '%s/slave_venv'" % curpath)
 
 with open("%s/slave_init.sh" % curpath, "w") as f:
     f.write("""#!/bin/sh
 
-. %s/slave_venv/bin/activate
+. '%s/slave_venv/bin/activate'
 
 # avoid to stuck raspberry cpus
 export OPENBLAS_NUM_THREADS=1
 export OPENBLAS_MAIN_FREE=1
 
-cd %s/slave
+cd '%s/slave'
 python3 find_master.py
 
 python3 main.py &
 """ % (curpath, curpath))
 
-os.system("chmod +x %s/slave_init.sh" % curpath)
+os.system("chmod +x '%s/slave_init.sh'" % curpath)
 
 if os.uname()[4][:3] == "arm":
-    os.system(""". %s/slave_venv/bin/activate && pip install https://github.com/humanfirewall-iot19/dlib-builds/raw/master/dlib-19.17.99-cp35-cp35m-linux_armv7l.whl && \
+    os.system(""". '%s/slave_venv/bin/activate' && pip install https://github.com/humanfirewall-iot19/dlib-builds/raw/master/dlib-19.17.99-cp35-cp35m-linux_armv7l.whl && \
 pip install gpiozero && pip install picamera && pip install rpi.gpio""" % curpath)
 
     with open(os.expanduser("~/.profile"), "a") as f:
-        f.write("\n%s/slave_init.sh\n" % curpath)
+        f.write("\n'%s/slave_init.sh'\n" % curpath)
 else:
-    os.system(""". %s/slave_venv/bin/activate && pip install https://github.com/humanfirewall-iot19/dlib-builds/raw/master/dlib-19.17.99-cp36-cp36m-linux_x86_64.whl && \
+    os.system(""". '%s/slave_venv/bin/activate' && pip install https://github.com/humanfirewall-iot19/dlib-builds/raw/master/dlib-19.17.99-cp36-cp36m-linux_x86_64.whl && \
 pip install opencv-python""" % curpath)
 
-os.system(". %s/slave_venv/bin/activate && pip install -r requirements.txt" % curpath)
+os.system(". '%s/slave_venv/bin/activate' && pip install -r requirements.txt" % curpath)
 
-os.system("cd %s && git clone https://github.com/humanfirewall-iot19/slave" % curpath)
+os.system("cd '%s' && git clone https://github.com/humanfirewall-iot19/slave" % curpath)
 
